ba6e0a8b37a7bb64c6b628cf4876bd3494f4f659
DennisRono <dennisrkibet@gmail.com>
2024-07-09 14:25:51 +0300

    completed the task asignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+let spy
+
+beforeEach(() => {
+  spy = sinon.spy(console, 'log')
+})
+
+afterEach(() => {
+  spy.restore()
+})
+
+const writeCards = (arr, surprise) => {
+  let messages = []
+  arr.forEach((user_name) => {
+    messages.unshift(
+      `Thank you, ${user_name}, for the wonderful ${surprise} gift!`
+    )
+  })
+  return messages
+}
+
+const countDown = (number) => {
+  while (number >= 0) {
+    console.log(number)
+    number--
+  }
+}
